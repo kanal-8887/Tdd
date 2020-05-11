82854c588abbce32c38f2e5c55a2fc592a97634f
kanal panchal <kanal.panchal@basilroot.com>
2020-05-11 12:40:51 +0530

    - multiple test cases added

--- LeapYearDemo/src/test/TestLeapYear.java
+++ LeapYearDemo/src/test/TestLeapYear.java
@@ -6,13 +6,61 @@ import org.junit.Test;
 import main.LeapYearValidator;
 
 public class TestLeapYear {
+	LeapYearValidator leapYear = new LeapYearValidator();
 
 	@Test
-	public void TestLeapYear(){
-		LeapYearValidator leapYear = new LeapYearValidator();
-		if (leapYear.isLeapYear(1990)) {
-			Assert.assertEquals(true, leapYear.isLeapYear(1990));
-		}
+	public void testLeapYear() {
+
 		Assert.assertEquals(true, leapYear.isLeapYear(2000));
+	}
+
+	@Test
+	public void testLeaveYear1700() {
+		Assert.assertEquals(false, leapYear.isLeapYear(1700));
+	}
+
+	@Test
+	public void testLeaveYear1800() {
+		Assert.assertEquals(false, leapYear.isLeapYear(1800));
+	}
+
+	@Test
+	public void testLeaveYear1900() {
+		Assert.assertEquals(false, leapYear.isLeapYear(1900));
+	}
+
+	@Test
+	public void testLeaveYear() {
+		Assert.assertEquals(false, leapYear.isLeapYear(2100));
+	}
+
+	@Test
+	public void testLeaveYear2008() {
+		Assert.assertEquals(false, leapYear.isLeapYear(2008));
+	}
+
+	@Test
+	public void testLeaveYear2012() {
+		Assert.assertEquals(false, leapYear.isLeapYear(2012));
+	}
+
+	@Test
+	public void testLeaveYear2016() {
+		Assert.assertEquals(false, leapYear.isLeapYear(2016));
+	}
+
+	@Test
+	public void testLeaveYearDividdBy2017() {
+		Assert.assertEquals(false, leapYear.isLeapYear(2017));
+	}
+
+	@Test
+	public void testLeaveYearDividdBy2018() {
+		Assert.assertEquals(false, leapYear.isLeapYear(2018));
+	}
+
+	@Test
+	public void testLeaveYearDividdBy2019() {
+		Assert.assertEquals(false, leapYear.isLeapYear(2019));
 	}
 }
